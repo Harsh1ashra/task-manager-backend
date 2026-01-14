d159931b02aa7a2562c9f3543d44263ad022e140
Harsh1ashra <harsh1.ashra@gmail.com>
2026-01-14 18:50:12 -0500

    Fixing cors

--- server.js
+++ server.js
@@ -11,6 +11,12 @@ app.use(cors());
 app.use(express.json());
 app.use("/tasks", taskRoutes);
 
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"]
+}));
+
+
 
 if (process.env.NODE_ENV !== "test") {
   mongoose
